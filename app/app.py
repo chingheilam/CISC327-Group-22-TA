d26a2b547ed9cd9311abc8a5c4a396078d28a9f5
mentlesoul <3144585698@qq.com>
2024-10-20 17:10:01 -0400

    Update vue3 new project with modules

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, request
+from flask_babel import Babel
+from flask_babel import gettext as _
 
 app = Flask(__name__)
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
+app.config['LANGUAGES'] = ['en', 'zh']
+
+
+def get_locale():
+    return request.args.get('lang', 'en')
+
+
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    ## return render_template('test.html', current_locale=get_locale())
     if request.method == 'POST':
         user_input = request.form['user_input']
         return render_template('index.html', user_input=user_input)
