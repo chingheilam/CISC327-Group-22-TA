5ae0f72a7e6d72ad5832af2d5169b60f7bd28796
chingheilam <168459239+chingheilam@users.noreply.github.com>
2024-10-22 15:00:42 -0400

    Index Page/ Flight Search and Flight Results UI & Logic

--- Assignment-2/src/main.js
+++ Assignment-2/src/main.js
@@ -1,14 +1,14 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import {createPinia} from 'pinia'
+import TDesign from 'tdesign-vue-next'; 
+import App from './App.vue';
+import 'tdesign-vue-next/es/style/index.css';  // Global TDesign styles
+import router from './router'; 
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const app = createApp(App);
 
-import App from './App.vue'
-import router from './router'
+app.use(createPinia());
+app.use(TDesign);  // Register TDesign globally
+app.use(router);
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
--- Assignment-2/src/router/index.js
+++ Assignment-2/src/router/index.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import FlightSearch from '../components/FlightSearch.vue';
+import FlightResults from '../components/FlightResults.vue';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes = [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+        path: '/',
+        name: 'FlightSearch',
+        component: FlightSearch
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+        path: '/flight-results',
+        name: 'FlightResults',
+        component: FlightResults
     }
-  ]
-})
+];
 
-export default router
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
