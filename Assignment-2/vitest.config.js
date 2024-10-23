3b7607c3254611b25454708425264c6256127c6c
chingheilam <168459239+chingheilam@users.noreply.github.com>
2024-10-23 00:01:27 -0400

    update

--- Assignment-2/vitest.config.js
+++ Assignment-2/vitest.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
+
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
+      setupFiles: './tests/setup.js',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
